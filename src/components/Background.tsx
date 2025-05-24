a566db7fb08aa519785ee8eb0b9e93b546b50137
Charlie Jeffries-Tipton <charliejt1992@hotmail.com>
2025-05-24 18:44:19 +0100

    fix background image

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,10 +1,12 @@
+import backgroundImage from "./assets/portfolio-background-image.png";
+
 const Background = () => (
   <>
     <div className="absolute inset-0 bg-gradient-to-br from-gray-700 via-black to-black opacity-70 z-10"></div>
     <div
       className="absolute inset-0 bg-cover bg-center bg-no-repeat z-0"
       style={{
-        backgroundImage: "url('./src/assets/portfolio-background-image.png')",
+        backgroundImage: `url(${backgroundImage})`,
       }}
     ></div>
   </>
